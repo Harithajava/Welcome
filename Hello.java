56351c798b126ca51479bdc5ee430afb0bc28834
Haritha <101461521+Harithajava@users.noreply.github.com>
2022-03-22 20:07:13 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+public class Hello
+{
+public void Hello()
+{
+
+}
+public static void main (String[]args)
+{
+ Hello h=new Hello();
+ int show_result=h.show(6,8);
+System.out.println(show_result);
+}
+
+public int show(int a,int b)
+{
+return a+b;
+}
+}
+
+
