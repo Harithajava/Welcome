60e057c4bb703e0f0d45020b5ff13283255c4ffd
Haritha <101461521+Harithajava@users.noreply.github.com>
2022-03-24 16:54:06 +0530

    Add files via upload

---
+++ While loop.java
@@ -0,0 +1,18 @@
+public class While loop
+{
+public static void main(String[]args)
+{
+int java=90;
+int phython=0;
+
+while(java<=95)
+{
+
+phython=phython+java;
+java++;
+
+
+}
+System.out.println("java is :"+phython);
+}
+}
