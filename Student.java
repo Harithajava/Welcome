afc0201e8dc84ebba2d27241591b10e38233fe50
Haritha <101461521+Harithajava@users.noreply.github.com>
2022-03-25 18:24:27 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,12 @@
+public class Student
+{
+int id;
+String name;
+public static void main(String[]args)
+{
+Student s=new Student();
+System.out.println(s);//prints object id
+System.out.println(s.id);
+System.out.println(s.name);
+}
+}
