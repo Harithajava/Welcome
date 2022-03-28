e0778c60f2182237188819a6ca82ca5024444059
Haritha <101461521+Harithajava@users.noreply.github.com>
2022-03-28 16:39:03 +0530

    Add files via upload

---
+++ Method2.java
@@ -0,0 +1,16 @@
+public class Method2
+{
+
+public static void main(String[]args)
+{
+int result=System.out.println(myHello(5));
+System.out.println(result);
+}
+static void Myhello(int x)
+{
+return x;
+}
+
+}
+
+
