5db03e9a39f5c25540da9774fa13b98e955a87fa
Haritha <101461521+Harithajava@users.noreply.github.com>
2022-03-29 16:27:01 +0530

    Add files via upload

---
+++ Welcome.java
@@ -0,0 +1,17 @@
+public class Welcome
+{
+public static void main(String[]args)
+{
+Welcome w=new Welcome();
+w.java();
+w.phython();
+}
+public void java()
+{
+System.out.println("hello java");
+}
+public void phython()
+{
+System.out.println("hello phython");
+}
+}
