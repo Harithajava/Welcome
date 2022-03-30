6df3f8c895f9fe42ec15826cb0cbea65610176ce
Haritha <101461521+Harithajava@users.noreply.github.com>
2022-03-30 19:16:19 +0530

    Add files via upload

---
+++ Fibonnaci.java
@@ -0,0 +1,12 @@
+public class Fibonnaci
+{
+public static void main(String[]args)
+{
+int no1=0;
+int no2=1;
+System.out.println(no1);
+System.out.println(no2);
+ int no3=no2+no1;
+System.out.println(no3);
+}
+}
