7bb399b6cd424acb65c6f2462f9e10628be74dc8
Haritha <101461521+Harithajava@users.noreply.github.com>
2022-04-11 18:20:17 +0530

    Add files via upload

---
+++ Multiplication of Number.java
@@ -0,0 +1,15 @@
+package Hello;
+import java.util.Scanner;
+public class Multiplication of Number
+{
+public static void main (String[]args)
+{
+Scanner s=new Scanner(System.in);
+System.out.println("Enter the number");
+int num=s.nextInt();
+for(int i=1;i<=10;i++)
+{
+System.out.println("The multiplication of five table:"+num*i);
+}
+}
+}
